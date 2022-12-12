b7112ca6ef5295b55ac5fec0b3487fc06c1d9e03
michaelSychev <107077803+michaelSychev@users.noreply.github.com>
2022-12-12 16:35:59 -0600

    Delete Kayboard directory
    Not applicable

--- Kayboard/src/keyboard/KeyLayout.java
+++
@@ -1,6 +0,0 @@
-package keyboard;
-
-public enum KeyLayout
-{
-    QWERTY, DVORAK, COLEMAK, ROTATION_13;
-}
--- Kayboard/src/keyboard/KeyboardMetrics.java
+++
@@ -1,7 +0,0 @@
-package keyboard;
-
-public interface KeyboardMetrics
-{
-    public double getDistance(Key key1, Key key2);
-    public double getDistance(String str);
-}
